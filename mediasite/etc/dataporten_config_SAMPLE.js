f3813a43d4b5ce16245f2fbd1acaccb525b2a478
Simon Skrødal <simon.skrodal@gmail.com>
2016-09-01 11:47:34 +0930

    ..

--- mediasite/etc/dataporten_config_SAMPLE.js
+++ mediasite/etc/dataporten_config_SAMPLE.js
@@ -1,6 +1,6 @@
 {
-	"user"		: "dataporten",
-	"passwd"	: "FROM_DASHBOARD",
+	"user"		    : "dataporten",
+	"passwd"	    : "FROM_DASHBOARD",
 	"group_id"      : "Dataporten Admin Group ID",
 	"group_invite"  : "Dataporten Admin Group URL",
 	"desc"          : "For api/mediasite"
