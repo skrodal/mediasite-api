a7fbf38bbc06d14ab88742f53a7392e8e5b2ba8d
Simon Skrødal <simon.skrodal@gmail.com>
2016-11-09 17:28:02 +1030

    Test

--- mediasite/api/scopes/basic.class.php
+++ mediasite/api/scopes/basic.class.php
@@ -75,22 +75,20 @@
 		public function storageList() {
 			// Last distinct orgs (hence last timestamp)
 
-
+/*
 			$response = $this->mySQLConnection->query("
 				SELECT storage_mib FROM $this->orgStorageTable
 				WHERE id IN (SELECT MAX(id) FROM $this->orgStorageTable GROUP BY org) 
 				ORDER BY storage_mib ASC
 			");
-/*
+*/
 
 			$response = $this->mySQLConnection->query("
-				SELECT storage_mib, MAX(id) 
+				SELECT storage_mib, timestamp 
 				FROM $this->orgStorageTable
-				GROUP BY org 
+				WHERE timestamp = SUBDATE(NOW(),1)
 				ORDER BY storage_mib ASC
 			");
-
-*/
 
 			$storage  = array();
 			foreach($response as $storage_mib) {
